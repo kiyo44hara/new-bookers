6980c0d549b860f2844c1b203476dd780613a0c6
EC2 Default User <ec2-user@ip-172-31-5-72.ap-northeast-1.compute.internal>
2023-02-06 14:51:59 +0000

    [評価機能実施しました]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
@@ -17,5 +17,12 @@ import "chartkick"
 import "chart.js"
 
 Rails.start()
-// Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
