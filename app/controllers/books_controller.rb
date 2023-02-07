03a742bd92b454e707374aee69780a428df1fe2e
EC2 Default User <ec2-user@ip-172-31-5-72.ap-northeast-1.compute.internal>
2023-02-07 13:17:57 +0000

    [タグを複数投稿できるようにしました]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,12 @@
 class BooksController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update, :destroy]
-  
-  def search_book
-    @book = Book.new
-    @books = Book.search(params[:keyword])
-  end
 
   def show
     @book_new = Book.new
     @book = Book.find(params[:id])
     current_user.view_counts.create(book_id: @book.id)
     @book_comment = BookComment.new
+    @book_tags = @book.tags
   end
 
   def index
@@ -23,13 +19,16 @@ class BooksController < ApplicationController
         @books = Book.rate_count
       else
         @books = Book.all.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+        @tag_list = Tag.all
       end
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    tag_list = params[:book][:name].split(',')
     if @book.save
+      @book.save_tag(tag_list)
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
@@ -39,11 +38,14 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @tag_list = @book.tags.pluck(:name).join(',')
   end
 
   def update
     @book = Book.find(params[:id])
+    tag_list = params[:book][:name].split(',')
     if @book.update(book_params)
+       @book.save_tag(tag_list)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
@@ -59,7 +61,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :rate, :category)
+    params.require(:book).permit(:title, :body, :rate)
   end
 
   def is_matching_login_user
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,8 @@ class Book < ApplicationRecord
   has_many :view_counts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+  has_many :book_tags,dependent: :destroy
+  has_many :tags,through: :book_tags
 
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
@@ -39,7 +41,18 @@ class Book < ApplicationRecord
     end
   end
   
-  def self.search(search_word)
-    Book.where(['category LIKE ?', "#{search_word}"])
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+    
+    old_tags.each do |old|
+      self.tags.delete Tag.find_by(name: old)
+    end
+    
+    new_tags.each do |new|
+      new_book_tag = Tag.find_or_create_by(name: new)
+      self.tags << new_book_tag
+    end
   end
 end
